b4b859273707d0d2c98f825cb96f7a71f107a33a
Emanuel-Lizarrondo <104107393+Emanuel-Lizarrondo@users.noreply.github.com>
2022-04-22 21:58:54 -0300

    nuevoo MIX

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 //Ejercicio 1
 
-let paso1: string = "mi paso";
+//let paso1: string = "mi paso";
 
 //Ejercicio 2
-let base: number = 5;
-let altura: number = 10;
-let resultado: number = base * altura;
+//let base: number = 5;
+//let altura: number = 10;
+//let resultado: number = base * altura;
 
-console.log(resultado);
+//console.log(resultado);
+
+let dato = document.getElementById("dato2");
+let btnEnv = document.getElementById("enviar");
+
+btbEnv.addEventListener("click", () => {
+  console.log(dato.value);
+  console.log(typeof dato.value);
+
+  let base = Number(dato.value);
+
+  console.log(typeof base);
+});
